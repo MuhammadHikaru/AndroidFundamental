436c195530e52c6a1b57fa605304afb1a12e3441
MuhammadHikaru <hikaruzahraan@gmail.com>
2020-02-04 13:31:30 +0700

    Cara Push Project ke GitHub

--- MasteringAndroid/app/src/main/java/com/example/masteringandroid1_layouting/BiodataActivity.java
+++ MasteringAndroid/app/src/main/java/com/example/masteringandroid1_layouting/BiodataActivity.java
@@ -1,14 +1,103 @@
 package com.example.masteringandroid1_layouting;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class BiodataActivity extends AppCompatActivity {
 
+    //Todo 1 : deklarasi view
+    EditText etnama;
+    EditText ettanggal;
+    EditText etkls;
+    EditText etjrsn;
+    EditText umur ;
+
+    String nama_lengkap,jurusan,kelas,tanggal;
+    int umur_skrg;
+
+    Intent kirimData;
+    Bundle packageBiodata;
+
+    AlertDialog.Builder builderAlert;
+    Button btview,bthapus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_biodata);
+
+        //Todo 2 : Insialisasi view
+        etnama = (EditText)findViewById(R.id.edt_nama);
+        ettanggal = (EditText)findViewById(R.id.edt_Tanggal);
+        etkls = (EditText)findViewById(R.id.edt_kelas);
+        etjrsn = (EditText)findViewById(R.id.edt_jurusan);
+
+
+
+        btview = (Button) findViewById(R.id.btn_lihat);
+        bthapus = (Button)findViewById(R.id.btn_hapus);
+
+        //Todo 3 : implementasi view
+        btview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              builderAlert = new AlertDialog.Builder(BiodataActivity.this);
+                nama_lengkap = etnama.getText().toString();
+                jurusan = etjrsn.getText().toString();
+                kelas = etkls.getText().toString();
+                tanggal = ettanggal.getText().toString();
+                umur_skrg = Integer.parseInt(umur.getText().toString());
+
+                Log.d("Log ", "nama lengkap : " + nama_lengkap + "jurusan : "+jurusan+", kelas : "+kelas+
+                        ", tanggal lahir : "+tanggal+", umur : "+umur);
+
+                builderAlert
+                      .setTitle("informasi")
+                      .setMessage("Data yang diinputkan adalah nama lengkap "+nama_lengkap+", "+"Tanggal lahir "
+                      + tanggal + ", kelas "+ kelas + "dan jurusan adalah "+ jurusan)
+                      .setPositiveButton("Kirim ", new DialogInterface.OnClickListener() {
+                          @Override
+                          public void onClick(DialogInterface dialog, int i) {
+                            packageBiodata = new Bundle();
+                            packageBiodata.putString("NAMA_LENGKAP", nama_lengkap);
+                             packageBiodata.putString("TANGGAL", tanggal);
+                             packageBiodata.putString("KELAS", kelas);
+                             packageBiodata.putString("JURUSAN",jurusan );
+                             packageBiodata.putInt("UMUR",umur_skrg);
+
+                             kirimData = new Intent(BiodataActivity.this,DetailBiodata.class);
+                             kirimData.putExtras(packageBiodata);
+                             startActivity(kirimData);
+
+                          }
+                      })
+                      .setNegativeButton("Batalkan ", new DialogInterface.OnClickListener() {
+                          @Override
+                          public void onClick(DialogInterface dialog, int which) {
+
+                          }
+                      });
+              AlertDialog alertDialog = builderAlert.create();
+              alertDialog.show();
+
+            }
+        });
+        bthapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etnama.setText("");
+                etjrsn.setText("");
+                etkls.setText("");
+                ettanggal.setText("");
+
+            }
+        });
     }
 }
--- MasteringAndroid/app/src/main/java/com/example/masteringandroid1_layouting/DetailBiodata.java
+++ MasteringAndroid/app/src/main/java/com/example/masteringandroid1_layouting/DetailBiodata.java
@@ -3,12 +3,37 @@ package com.example.masteringandroid1_layouting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailBiodata extends AppCompatActivity {
+
+    TextView tpnama;
+    TextView tptanggal;
+    TextView tpkelas;
+    TextView tpjurusan;
+    TextView tpumur;
+    int umur;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_biodata);
+
+        tpnama =(TextView) findViewById(R.id.nama);
+        tptanggal =(TextView) findViewById(R.id.tanggal);
+        tpkelas =(TextView) findViewById(R.id.kelas);
+        tpjurusan =(TextView) findViewById(R.id.jurusan);
+        tpumur =(TextView)findViewById(R.id.umur);
+
+        Bundle getBiodata = getIntent().getExtras();
+        tpnama.setText(getBiodata.getString("NAMA_LENGKAP"));
+        tptanggal.setText(getBiodata.getString("TANGGAL"));
+        tpkelas.setText(getBiodata.getString("KELAS"));
+        tpjurusan.setText(getBiodata.getString("JURUSAN"));
+        tpumur.setText(String.valueOf(umur));
+        umur = getBiodata.getInt("UMUR");
+
+
     }
 }
