dd56d28ce9aaf4680fac7259f989d6786df83123
MuhammadHikaru <hikaruzahraan@gmail.com>
2020-02-05 14:41:39 +0700

    cara membuat aplikasi menggunakan webview

---
+++ appsweb/app/src/main/java/commanderhikaru/co/id/herohelper/HeroHelper.java
@@ -0,0 +1,13 @@
+package commanderhikaru.co.id.herohelper;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HeroHelper extends AppCompatActivity {
+    public void intentpindah(Activity activity_awal, Class activity_tujuan){
+        Intent pindah = new Intent(activity_awal, activity_tujuan);
+        activity_awal.startActivity(pindah);
+    }
+}
